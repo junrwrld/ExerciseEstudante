c2872b3a914f1513b6e4252f8a3fb6213374b91e
João Junior <batistasousajoao@gmail.com>
2023-11-07 11:33:06 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciseEstudante/Estudante.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace ExerciseEstudante
+{
+    class Estudante
+    {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudante( string nome, string email) { 
+            
+            Nome = nome;
+            Email = email;  
+
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+
+    }
+}
